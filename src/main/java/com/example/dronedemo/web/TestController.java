3734b1420575143b64e6204465d2a1cf650f4762
haiyang <haiyang@uwantsoft.com>
2022-11-02 17:29:21 +0800

    init 26

--- src/main/java/com/example/dronedemo/web/TestController.java
+++ src/main/java/com/example/dronedemo/web/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @GetMapping(value = "/helloworld")
     protected String messageCallback(String data, HttpServletResponse response) throws Exception {
         PrintWriter out = response.getWriter();
-        out.print("hello worldABC: " + data);
+        out.print("hello world DEF: " + data);
         out.flush();
         return null;
     }
