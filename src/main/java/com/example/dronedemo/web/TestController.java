b1c0323aa2427a85453aa0824a458a9b642b9cd7
haiyang <haiyang@uwantsoft.com>
2022-11-09 13:45:06 +0800

    init a 70

--- src/main/java/com/example/dronedemo/web/TestController.java
+++ src/main/java/com/example/dronedemo/web/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 
 /**
  * @author Zhou haiyang
@@ -15,7 +16,9 @@ public class TestController {
     @GetMapping(value = "/helloworld")
     protected String messageCallback(String data, HttpServletResponse response) throws Exception {
         PrintWriter out = response.getWriter();
-        out.print("hello world YYY: " + data);
+        InetAddress host = InetAddress.getLocalHost();// 获得本机的InetAddress对象
+        String hostAddress = host.getHostAddress();// 获得本机的IP地址
+        out.print("hello world[" + hostAddress + "] YYY: " + data);
         out.flush();
         return null;
     }
