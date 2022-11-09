3d9729776366ebefdd9bddd1244316b699cd324b
haiyang <haiyang@uwantsoft.com>
2022-11-09 14:02:37 +0800

    init a 71

--- src/main/java/com/example/dronedemo/web/TestController.java
+++ src/main/java/com/example/dronedemo/web/TestController.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 /**
  * @author Zhou haiyang
@@ -16,11 +20,32 @@ public class TestController {
     @GetMapping(value = "/helloworld")
     protected String messageCallback(String data, HttpServletResponse response) throws Exception {
         PrintWriter out = response.getWriter();
-        InetAddress host = InetAddress.getLocalHost();// 获得本机的InetAddress对象
-        String hostAddress = host.getHostAddress();// 获得本机的IP地址
+        String hostAddress = getInet4Address();// 获得本机的IP地址
         out.print("hello world[" + hostAddress + "] YYY: " + data);
         out.flush();
         return null;
+    }
+
+    public static String getInet4Address() {
+        Enumeration nis;
+        String ip = null;
+        try {
+            nis = NetworkInterface.getNetworkInterfaces();
+            for (; nis.hasMoreElements(); ) {
+                NetworkInterface ni = (NetworkInterface) nis.nextElement();
+                Enumeration ias = ni.getInetAddresses();
+                for (; ias.hasMoreElements(); ) {
+                    InetAddress ia = (InetAddress) ias.nextElement();
+                    if (ia instanceof Inet4Address && !ia.getHostAddress().equals("127.0.0.1")) {
+                        ip = ia.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return ip;
+
     }
 
 }
